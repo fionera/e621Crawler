b1452ac8c65dbf0c17559848dcb35adb6f1a530c
Cat™ <31106839+CatTheHacker@users.noreply.github.com>
2020-08-28 14:54:00 +0200

    Refactor

--- api/list.go
+++ api/list.go
@@ -2,57 +2,93 @@ package api
 
 import (
 	"encoding/json"
-	"github.com/go-resty/resty"
 	"io/ioutil"
 	"strconv"
+	
+	"github.com/sirupsen/logrus"
+	"github.com/go-resty/resty"
 )
 
 const (
-	listUrl  = "https://e621.net/post/index.json"
+	listUrl  = "https://e621.net/posts.json"
 	maxLimit = 320
 )
 
-type Posts []Post
-
-type Post struct {
-	Artist    []string `json:"artist"`
-	Author    string   `json:"author"`
-	Change    int      `json:"change"`
-	Children  string   `json:"children"`
-	CreatedAt struct {
-		JSONClass string `json:"json_class"`
-		N         int    `json:"n"`
-		S         int    `json:"s"`
-	} `json:"created_at"`
-	CreatorID     int         `json:"creator_id"`
-	Description   string      `json:"description"`
-	FavCount      int         `json:"fav_count"`
-	FileExt       string      `json:"file_ext"`
-	FileSize      int         `json:"file_size"`
-	FileURL       string      `json:"file_url"`
-	HasChildren   bool        `json:"has_children"`
-	HasComments   bool        `json:"has_comments"`
-	HasNotes      bool        `json:"has_notes"`
-	Height        int         `json:"height"`
-	ID            int         `json:"id"`
-	LockedTags    interface{} `json:"locked_tags"`
-	Md5           string      `json:"md5"`
-	ParentID      int         `json:"parent_id"`
-	PreviewHeight int         `json:"preview_height"`
-	PreviewURL    string      `json:"preview_url"`
-	PreviewWidth  int         `json:"preview_width"`
-	Rating        string      `json:"rating"`
-	SampleHeight  int         `json:"sample_height"`
-	SampleURL     string      `json:"sample_url"`
-	SampleWidth   int         `json:"sample_width"`
-	Score         int         `json:"score"`
-	Source        string      `json:"source"`
-	Sources       []string    `json:"sources"`
-	Status        string      `json:"status"`
-	Tags          string      `json:"tags"`
-	Width         int         `json:"width"`
+type Posts struct {
+	Posts []Post `json:"posts"`
 }
-
+type File struct {
+	Width  int    `json:"width"`
+	Height int    `json:"height"`
+	Ext    string `json:"ext"`
+	Size   int    `json:"size"`
+	Md5    string `json:"md5"`
+	URL    string `json:"url"`
+}
+type Preview struct {
+	Width  int    `json:"width"`
+	Height int    `json:"height"`
+	URL    string `json:"url"`
+}
+type Sample struct {
+	Has    bool   `json:"has"`
+	Height int    `json:"height"`
+	Width  int    `json:"width"`
+	URL    string `json:"url"`
+}
+type Score struct {
+	Up    int `json:"up"`
+	Down  int `json:"down"`
+	Total int `json:"total"`
+}
+type Tags struct {
+	General   []string      `json:"general"`
+	Species   []string      `json:"species"`
+	Character []string      `json:"character"`
+	Copyright []string      `json:"copyright"`
+	Artist    []string      `json:"artist"`
+	Invalid   []interface{} `json:"invalid"`
+	Lore      []interface{} `json:"lore"`
+	Meta      []string      `json:"meta"`
+}
+type Flags struct {
+	Pending      bool `json:"pending"`
+	Flagged      bool `json:"flagged"`
+	NoteLocked   bool `json:"note_locked"`
+	StatusLocked bool `json:"status_locked"`
+	RatingLocked bool `json:"rating_locked"`
+	Deleted      bool `json:"deleted"`
+}
+type Relationships struct {
+	ParentID          interface{}   `json:"parent_id"`
+	HasChildren       bool          `json:"has_children"`
+	HasActiveChildren bool          `json:"has_active_children"`
+	Children          []interface{} `json:"children"`
+}
+type Post struct {
+	ID            int           `json:"id"`
+	CreatedAt     string        `json:"created_at"`
+	UpdatedAt     string        `json:"updated_at"`
+	File          File          `json:"file"`
+	Preview       Preview       `json:"preview"`
+	Sample        Sample        `json:"sample"`
+	Score         Score         `json:"score"`
+	Tags          Tags          `json:"tags"`
+	LockedTags    []interface{} `json:"locked_tags"`
+	ChangeSeq     int           `json:"change_seq"`
+	Flags         Flags         `json:"flags"`
+	Rating        string        `json:"rating"`
+	FavCount      int           `json:"fav_count"`
+	Sources       []string      `json:"sources"`
+	Pools         []interface{} `json:"pools"`
+	Relationships Relationships `json:"relationships"`
+	ApproverID    int           `json:"approver_id"`
+	UploaderID    int           `json:"uploader_id"`
+	Description   string        `json:"description"`
+	CommentCount  int           `json:"comment_count"`
+	IsFavorited   bool          `json:"is_favorited"`
+	HasNotes      bool          `json:"has_notes"`
+}
 func List(limit int, beforeId int, page int, tags string, typedTags bool) (Posts, error) {
 	queryParams := map[string]string{}
 	if limit != 0 {
@@ -74,15 +110,14 @@ func List(limit int, beforeId int, page int, tags string, typedTags bool) (Posts
 		queryParams["typed_tags"] = strconv.FormatBool(typedTags)
 	}
 	client := resty.New()
-	resp, err := client.R().
-		SetQueryParams(queryParams).
-		SetHeader("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36").
-		SetHeader("cookie", "__cfduid=dfdd28cc2534b56b8e878dd540944bf221555100794; blacklist_avatars=true; blacklist_users=false; css=hexagon%3Boverrides%2Fchristmas%3B1462320000; mode=view; e621=BAh7CDoPc2Vzc2lvbl9pZCIlMzZhMzcwMjc4YmMxMmEzZDM0YmRlNTA4ZmU2ZGI2MzA6EF9jc3JmX3Rva2VuSSIxR0pkMXBqMWxJMVZlcWFHQmk1bkNHNlcwY2JzcGtkQ0tUc0xVaTltZGhHaz0GOgZFRkkiCmZsYXNoBjsHRklDOidBY3Rpb25Db250cm9sbGVyOjpGbGFzaDo6Rmxhc2hIYXNoewAGOgpAdXNlZHsA--62ec4afec78a2df7a9934f6dc6ea8c20c8731da4; cf_clearance=42717a4c94b201952cb6f6b4bb38a874a6808c26-1555114721-2592000-250").
-		SetHeader("Accept", "application/json").
-		Get(listUrl)
+	client.SetHeaders(map[string]string{
+		"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
+		"Accept":     "application/json",
+	})
+	resp, err := client.R().SetQueryParams(queryParams).Get(listUrl)
 
 	if err != nil {
-		return nil, err
+		logrus.Fatal(err)
 	}
 
 	_ = ioutil.WriteFile("api/before_id_"+strconv.Itoa(beforeId)+".json", resp.Body(), 0755)
@@ -90,7 +125,7 @@ func List(limit int, beforeId int, page int, tags string, typedTags bool) (Posts
 	var posts Posts
 	err = json.Unmarshal(resp.Body(), &posts)
 	if err != nil {
-		return nil, err
+		logrus.Fatal(err)
 	}
 
 	return posts, nil
